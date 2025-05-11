e386629271d5d1ee6fd637dbb0315c747e1139a5
Danish <dkhans2001@gmail.com>
2025-05-10 19:30:07 -0500

    function

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 
+// Function 1: calculateTax
+function calculateTax(amount) {
+  return amount * 0.10;
+}
+
+// Function 2: convertToUpperCase
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+// Function 4: isPalindrome
+function isPalindrome(word) {
+  const normalized = word.toLowerCase();
+  const reversed = normalized.split('').reverse().join('');
+  return normalized === reversed;
+}
+
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discountAmount = (originalPrice * discountPercentage) / 100;
+  return originalPrice - discountAmount;
+}
 
 
 
